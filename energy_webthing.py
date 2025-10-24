6dc1a7154c12a8105b9c96cfa475de340dd09898
grro <gregror.roth@1und1.de>
2025-10-24 07:41:29 +0200

    refactoring & mcp support

--- energy_webthing.py
+++ energy_webthing.py
@@ -26,7 +26,7 @@ def run_server(port: int,
     server = WebThingServer(MultipleThings([ProviderThing(provider), PvThing(pv), BatteryThing(battery), EnergyThing(energy)], "energy"), port=port, disable_host_validation=True)
 
     try:
-        logging.info('starting the server http://localhost:' + str(port) + " (provider meter=" + meter_addr_provider + ")")
+        logging.info('starting the server http://' + server.hostname + ':' + str(port) + " (provider meter=" + meter_addr_provider + ")")
         provider.start()
         pv.start()
         battery.start()
