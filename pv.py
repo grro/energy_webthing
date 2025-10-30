964c1fc4eca5633ca651faa82df41b8fcf495411
grro <gregror.roth@1und1.de>
2025-10-30 22:08:09 +0100

    logging improved

--- pv.py
+++ pv.py
@@ -165,7 +165,7 @@ class Pv:
 
     def start(self):
         Thread(target=self.__measure_loop, daemon=True).start()
-        Thread(target=self.__info_loop, daemon=True).start()
+        #Thread(target=self.__info_loop, daemon=True).start()
 
     def stop(self):
         self.__is_running = False
