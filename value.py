aeb370683b18dba2b90f57a7de588be446fc3ab0
grro <gregror.roth@1und1.de>
2025-10-31 14:25:25 +0100

    processing optimized

---
+++ value.py
@@ -0,0 +1,17 @@
+from datetime import datetime, timedelta
+
+
+class Value:
+
+    def __init__(self, window_sec: int = 5):
+        self.__window_sec = window_sec
+        self.__value = 0
+        self.__last_fetched_value = 0
+        self.__last_fetch_date = datetime.now()
+
+    def set_and_get(self, new_value: int):
+        now = datetime.now()
+        if self.__last_fetch_date + timedelta(seconds = self.__window_sec) < now:
+            self.__last_fetch_date = now
+            self.__last_fetched_value = self.__value
+        return self.__last_fetched_value
