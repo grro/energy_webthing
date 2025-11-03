5c07623de9f4745fd550f6aa02480602badadb3b
grro <gregror.roth@1und1.de>
2025-11-03 06:58:07 +0100

    surplus gross added

--- energy_mcp.py
+++ energy_mcp.py
@@ -1,6 +1,6 @@
-from time import sleep
 from mcp_server import MCPServer
 from energy import Energy
+from pv import Pv
 
 
 
@@ -8,9 +8,10 @@ from energy import Energy
 
 class EnergyMCPServer(MCPServer):
 
-    def __init__(self,port: int, energy: Energy):
+    def __init__(self,port: int, energy: Energy, pv: Pv):
         super().__init__("energy", port)
         self.energy = energy
+        self.pv = pv
 
         @self.mcp.resource("resource://provider_power", description="Current provider power. May be negative")
         def get_provider_power() -> int:
@@ -176,9 +177,13 @@ class EnergyMCPServer(MCPServer):
         def get_power_surplus_5m() -> int:
             return energy.power_surplus_5m
 
-        @self.mcp.resource("resource://power_surplus_peek_hour", description="The hour of the day when the surplus peak currently occurs")
-        def get_power_surplus_peek_hour() -> int:
-            return energy.power_surplus_peek_hour
+        @self.mcp.resource("resource://power_surplus_gross_1m", description="Current power surplus gross (downstream pv + downstream battery) (smoothen 5 min)")
+        def get_power_surplus_gross_1m() -> int:
+            return energy.power_surplus_gross_1m
+
+        @self.mcp.resource("resource://power_peek_hour", description="The hour of the day when the pv peak currently occurs (UTC)")
+        def get_power_peek_hour() -> int:
+            return pv.power_peek_hour_utc
 
 
 
