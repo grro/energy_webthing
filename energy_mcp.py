31b602a5b443af4dda433d38b7c00d2da3e1d695
grro <gregror.roth@1und1.de>
2026-04-05 19:49:03 +0200

    mqtt support added

--- energy_mcp.py
+++ energy_mcp.py
@@ -28,7 +28,7 @@ class EnergyMCPServer(MCPServer):
                     f"- Grid Power: {self.energy.provider.provider_power} W (Positive: Buying/Import | Negative: Exporting)\n"
                     f"- PV Production: {self.energy.pv.power_downstream} W (Current solar generation)\n"
                     f"- Expected PV Daily Peek hour: {self.energy.pv.power_peak_hour_utc} UTC (latest " + ", ".join([str(h) for h in self.energy.pv.latest_peeks_hour_utc()]) + " UTC)\n"
-                    f"- Battery Level: {self.energy.battery.state_of_charge} W (Power level in %, Total capacity 1,92 kWh)\n"
+                    f"- Battery Level: {self.energy.battery.state_of_charge} W (Power level in %, Total capacity 1,92 kWh; minimum b attery level is 10%)\n"
                     f"- Battery Discharge: {self.energy.battery.power_downstream} W (Power usage from battery)\n"
                     f"- Battery Charge: {self.energy.battery.power_upstream} W (Power flowing into battery)\n"
                     f"- Total Consumption: {self.energy.power_consumption} W (Current household load, excluding battery charging)\n"
